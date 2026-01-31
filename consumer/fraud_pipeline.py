063a4a36489b7cd329b2a327be0d1a43736117c7
Aditya Sahni <95126292+sahniaditya007@users.noreply.github.com>
2026-01-31 13:11:18 +0530

    feat: add Kafka fraud pipeline consumer skeleton

---
+++ consumer/fraud_pipeline.py
@@ -0,0 +1,71 @@
+import json
+import time
+from datetime import datetime
+
+from confluent_kafka import Consumer, KafkaException, KafkaError
+
+# -----------------------------
+# Kafka configuration
+# -----------------------------
+KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
+TOPIC_NAME = "upi.transaction.raw"
+CONSUMER_GROUP = "fraud-detector-v1"
+
+consumer_conf = {
+    "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
+    "group.id": CONSUMER_GROUP,
+    "auto.offset.reset": "earliest",
+    "enable.auto.commit": True,
+}
+
+consumer = Consumer(consumer_conf)
+
+# -----------------------------
+# Processing stub
+# -----------------------------
+def process_event(event, metadata):
+    print(
+        f"[EVENT] payer={event['payer_upi_id']} "
+        f"payee={event['payee_upi_id']} "
+        f"amount={event['amount']} "
+        f"partition={metadata['partition']} "
+        f"offset={metadata['offset']}"
+    )
+    
+#Main
+def run():
+    print("Starting fraud pipeline consumer...")
+    consumer.subscribe([TOPIC_NAME])
+    
+    try:
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            
+            if msg is None:
+                continue
+            
+            if msg.error():
+                if msg.error().code() == KafkaError._PATITION_EOF:
+                    continue
+                else:
+                    raise KafkaException(msg.error())
+            
+            event = json.loads(msg.value().decode("utf-8"))
+            
+            metadata = {
+                "topic": msg.topic(),
+                "partition": msg.partition(),
+                "offset": msg.offset(),
+                "timestamp": msg.timestamp(),
+            }
+            
+            process_event(event, metadata)
+    
+    except KeyboardInterrupt:
+        print("\n Stopping Consumer...")
+    
+    finally:
+        consumer.close()
+
+if __name__ == "__main__":
+    run()
